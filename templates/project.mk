480807143c465dab9add7d4389b6d6d7acadb2b4
boochow <boochowp@gmail.com>
2025-03-15 02:10:44 +0900

    include the heap-size estimation in project.mk

--- templates/project.mk
+++ templates/project.mk
@@ -2,7 +2,28 @@
 # Project Customization
 # #############################################################################
 
+.DEFAULT_GOAL := all
+
 PROJECT = {{patch_name}}
+
+# The minimum heap size
+# HEAP_SIZE ?= {{heap_size}}
+
+ifndef HEAP_SIZE
+# Estimate the necessary heap size and store it into HEAP_SIZE_FILE
+HEAP_SIZE_FILE := logue_heap_size.mk
+
+$(HEAP_SIZE_FILE):
+	$(MAKE) -f Makefile.testmem
+	@TMPFILE=$$(mktemp) ; \
+	./testmem > $$TMPFILE ; \
+	HEAP_SIZE_TESTMEM=$$(grep '^total:' $$TMPFILE | awk '{print $$2}') ; \
+	echo "HEAP_SIZE := $$HEAP_SIZE_TESTMEM" > $(HEAP_SIZE_FILE) ; \
+	rm -f $$TMPFILE ; \
+	$(MAKE) -f Makefile.testmem
+
+include $(HEAP_SIZE_FILE)
+endif
 
 UCSRC = logue_mem.c {{heavy_files_c}}
 
@@ -10,7 +31,7 @@ UCXXSRC = logue_heavy.cpp {{heavy_files_cpp}}
 
 UINCDIR = 
 
-UDEFS = -DNDEBUG -DUNIT_HEAP_SIZE={{heap_size}} -fvisibility=hidden
+UDEFS = -DNDEBUG -DUNIT_HEAP_SIZE=$(HEAP_SIZE) -fvisibility=hidden
 
 # Try disabling this option when the results are got inaccurate.
 UDEFS += -DLOGUE_FAST_MATH
